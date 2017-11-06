7e6f4fdd3259551ddca2e0c4fa62a874b18d441c
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-06 21:09:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,11 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade )
+    @id = id
     @name = name
     @grade = grade
-    @id = id
+    
   end
 
   def self.create_table
