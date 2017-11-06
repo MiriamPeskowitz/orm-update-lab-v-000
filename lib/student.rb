21825533af3d39b6b0b6bd64bc240271a10a60b5
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-06 21:26:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,10 +51,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = Student.new(name, grade)
+    
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
+    student = Student.new(name, grade)
   end
 
   def self.find_by_name(name)
